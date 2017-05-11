0f741f601fa52b14791e2cfebb78e5a94dcf773c
mguillau42 <mguillau@student.42.fr>
2017-05-11 06:50:53 -0400

    PT_LOAD method proved inefficient. Discontinued

--- source/main.c
+++ source/main.c
@@ -5,32 +5,34 @@
 #define CODE_OFFSET (phdr->p_offset + phdr->p_memsz)
 #define CODE_ADRESS (START_ADRESS + CODE_OFFSET)
 
-void				inject(void *m, struct stat *buf)
+void				inject(void *m)
 {
 	Elf64_Ehdr		*ehdr;
 	Elf64_Phdr		*phdr;
+	Elf64_Phdr		*next;
 
 	ehdr = (void*)m;
-	phdr = (void*)f_mmaped + ehdr->e_phoff;
+	phdr = (void*)m + ehdr->e_phoff;
 	printf("[+] Find free space\n");
-	for(i = 0; i < ehdr->e_phnum - 1; i++)
+	for(int i = 0; i < ehdr->e_phnum - 1; i++)
 	{
 		if(phdr->p_type == PT_LOAD)
 			break;
 		phdr++;
 	}
+	next = phdr + 1;
 	if(next->p_type != PT_LOAD || phdr->p_type != PT_LOAD)
 	{
 		printf("[!] Didn't find two PT_LOAD\n");
 		return ;
 	}
 
-	if(phdr->p_memsz != phdr->p_filesz || (CODE_OFFSET + 47) > (next->p_offset + phdr->p_offset))
+	if(phdr->p_memsz != phdr->p_filesz || (CODE_OFFSET + 256) > (next->p_offset + phdr->p_offset))
 	{
 		printf("[!] Didn't find enough free space\n");
 		return ;
 	}
-	printf("[+] Free space found : %d bytes.n", (next->p_offset) - (CODE_OFFSET));
+	printf("[+] Free space found : %lu bytes.\n", (next->p_offset) - (CODE_OFFSET));
 }
 
 int					main(int ac, char **av)
@@ -65,6 +67,7 @@ int					main(int ac, char **av)
 	}
 
 	// check ELF64
+	p = m;
 	if (ft_memcmp(ELFMAG, m, SELFMAG) && p[EI_CLASS] != ELFCLASS64)
 	{
 		printf("[!] Provided binary file isn't an ELF64\n");
