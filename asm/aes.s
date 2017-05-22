52c28adad9401d8dcd35ba1306e1c489558983a9
Florian VENTURINI <fventuri@student.42.fr>
2017-05-22 14:22:29 +0200

    ENCRYPTION/DECRYPTION OK :tada:

--- asm/aes.s
+++ asm/aes.s
@@ -1,45 +1,41 @@
 section .text
 	global aes
+	global unaes
 
-; ENCRYPT
-aes:
-	push rbp
-	mov rbp, rsp
-	push rdi				; saave pointer to data
-	; STORE ROUND KEYS IN xmm0-10
-	movdqu xmm11, [rdi]
+; Fills registers xmm0-10 with the round keys
+key_expansion:
 	movdqu xmm0, xmm11					; first key
-	aeskeygenassist xmm12, xmm11, 0x1
+	aeskeygenassist xmm12, xmm0, 0x1
 	call key_expansion_128
 	movdqu xmm1, xmm11					; add key
-	aeskeygenassist xmm12, xmm11, 0x2
+	aeskeygenassist xmm12, xmm1, 0x2
 	call key_expansion_128
 	movdqu xmm2, xmm11					; add key
-	aeskeygenassist xmm12, xmm11, 0x4
+	aeskeygenassist xmm12, xmm2, 0x4
 	call key_expansion_128
 	movdqu xmm3, xmm11					; add key
-	aeskeygenassist xmm12, xmm11, 0x8
+	aeskeygenassist xmm12, xmm3, 0x8
 	call key_expansion_128
 	movdqu xmm4, xmm11					; add key
-	aeskeygenassist xmm12, xmm11, 0x10
+	aeskeygenassist xmm12, xmm4, 0x10
 	call key_expansion_128
 	movdqu xmm5, xmm11					; add key
-	aeskeygenassist xmm12, xmm11, 0x20
+	aeskeygenassist xmm12, xmm5, 0x20
 	call key_expansion_128
 	movdqu xmm6, xmm11					; add key
-	aeskeygenassist xmm12, xmm11, 0x40
+	aeskeygenassist xmm12, xmm6, 0x40
 	call key_expansion_128
 	movdqu xmm7, xmm11					; add key
-	aeskeygenassist xmm12, xmm11, 0x80
+	aeskeygenassist xmm12, xmm7, 0x80
 	call key_expansion_128
 	movdqu xmm8, xmm11					; add key
-	aeskeygenassist xmm12, xmm11, 0x1b
+	aeskeygenassist xmm12, xmm8, 0x1b
 	call key_expansion_128
 	movdqu xmm9, xmm11					; add key
-	aeskeygenassist xmm12, xmm11, 0x36
+	aeskeygenassist xmm12, xmm9, 0x36
 	call key_expansion_128
 	movdqu xmm10, xmm11					; add key
-	jmp aes_encrypt
+	ret
 
 key_expansion_128:
 	pshufd xmm12, xmm12, 0xff
@@ -52,7 +48,14 @@ key_expansion_128:
 	pxor xmm11, xmm12
 	ret
 
-aes_encrypt:
+; ENCRYPT
+aes:
+	push rbp
+	mov rbp, rsp
+	push rdi				; save pointer to data
+	movdqu xmm11, [rsi]
+	movdqu xmm15, [rdi]
+	call key_expansion
 	; ENCRYPTION SEQUENCE
 	pxor xmm15, xmm0 ; Whitening step (Round 0)
 	aesenc xmm15, xmm1		; Round 1
@@ -70,7 +73,48 @@ aes_encrypt:
 	pextrq r8, xmm15, 0		; extracts bits 0-63 and stores them in r8
 	pextrq r9, xmm15, 1		; extracts bits 64-127 and stores them in r9
 	pop rax					; get back ptr to data
-	mov [rax+8], r8			; writes encrypted bytes in memory
-	mov [rax], r9		; ^
+	mov [rax], r8			; writes encrypted bytes in memory
+	mov [rax + 8], r9		; ^
 	leave
 	ret
+
+; DECRYPT
+unaes:
+	push rbp
+	mov rbp, rsp
+	push rdi				; saave pointer to data
+	; STORE ROUND KEYS IN xmm0-10
+	movdqu xmm11, [rsi]
+	movdqu xmm15, [rdi]
+	call key_expansion
+	; DECRYPTION SEQUENCE
+	pxor xmm15, xmm10 ; Whitening step (Round 0)
+	aesimc xmm9, xmm9
+	aesdec xmm15, xmm9		; Round 1
+	aesimc xmm8, xmm8
+	aesdec xmm15, xmm8		; Round 2
+	aesimc xmm7, xmm7
+	aesdec xmm15, xmm7		; Round 3
+	aesimc xmm6, xmm6
+	aesdec xmm15, xmm6		; Round 4
+	aesimc xmm5, xmm5
+	aesdec xmm15, xmm5		; Round 5
+	aesimc xmm4, xmm4
+	aesdec xmm15, xmm4		; Round 6
+	aesimc xmm3, xmm3
+	aesdec xmm15, xmm3		; Round 7
+	aesimc xmm2, xmm2
+	aesdec xmm15, xmm2		; Round 8
+	aesimc xmm1, xmm1
+	aesdec xmm15, xmm1		; Round 9
+	aesdeclast xmm15, xmm0	; Round 10
+
+	; CHANGE DATA IN MEMORY AND RETURNS
+	pextrq r8, xmm15, 0		; extracts bits 0-63 and stores them in r8
+	pextrq r9, xmm15, 1		; extracts bits 64-127 and stores them in r9
+	pop rax					; get back ptr to data
+	mov [rax], r8			; writes encrypted bytes in memory
+	mov [rax + 8], r9		; ^
+	leave
+	ret
+
--- asm/main.c
+++ asm/main.c
@@ -9,38 +9,22 @@ uint8_t	*set_key(void)
 {
 	uint8_t	*ret = malloc(16);
 
-	/* ret[0] = 0x0;*/
-	/* ret[1] = 0x1;*/
-	/* ret[2] = 0x2;*/
-	/* ret[3] = 0x3;*/
-	/* ret[4] = 0x4;*/
-	/* ret[5] = 0x5;*/
-	/* ret[6] = 0x6;*/
-	/* ret[7] = 0x7;*/
-	/* ret[8] = 0x8;*/
-	/* ret[9] = 0x9;*/
-	/* ret[10] = 0xa;*/
-	/* ret[11] = 0xb;*/
-	/* ret[12] = 0xc;*/
-	/* ret[13] = 0xd;*/
-	/* ret[14] = 0xe;*/
-	/* ret[15] = 0xf;*/
-	ret[15] = 0x0;
-	ret[14] = 0x1;
-	ret[13] = 0x2;
-	ret[12] = 0x3;
-	ret[11] = 0x4;
-	ret[10] = 0x5;
-	ret[9] = 0x6;
-	ret[8] = 0x7;
-	ret[7] = 0x8;
-	ret[6] = 0x9;
-	ret[5] = 0xa;
-	ret[4] = 0xb;
-	ret[3] = 0xc;
-	ret[2] = 0xd;
-	ret[1] = 0xe;
-	ret[0] = 0xf;
+	ret[0] = 0x0;
+	ret[1] = 0x1;
+	ret[2] = 0x2;
+	ret[3] = 0x3;
+	ret[4] = 0x4;
+	ret[5] = 0x5;
+	ret[6] = 0x6;
+	ret[7] = 0x7;
+	ret[8] = 0x8;
+	ret[9] = 0x9;
+	ret[10] = 0xa;
+	ret[11] = 0xb;
+	ret[12] = 0xc;
+	ret[13] = 0xd;
+	ret[14] = 0xe;
+	ret[15] = 0xf;
 	return (ret);
 }
 
@@ -48,38 +32,22 @@ uint8_t	*set_data(void)
 {
 	uint8_t	*ret = malloc(16);
 
-	/* ret[0] = 0x00;*/
-	/* ret[1] = 0x11;*/
-	/* ret[2] = 0x22;*/
-	/* ret[3] = 0x33;*/
-	/* ret[4] = 0x44;*/
-	/* ret[5] = 0x55;*/
-	/* ret[6] = 0x66;*/
-	/* ret[7] = 0x77;*/
-	/* ret[8] = 0x88;*/
-	/* ret[9] = 0x99;*/
-	/* ret[10] = 0xaa;*/
-	/* ret[11] = 0xbb;*/
-	/* ret[12] = 0xcc;*/
-	/* ret[13] = 0xdd;*/
-	/* ret[14] = 0xee;*/
-	/* ret[15] = 0xff;*/
-	ret[15] = 0x00;
-	ret[14] = 0x11;
-	ret[13] = 0x22;
-	ret[12] = 0x33;
-	ret[11] = 0x44;
-	ret[10] = 0x55;
-	ret[9] = 0x66;
-	ret[8] = 0x77;
-	ret[7] = 0x88;
-	ret[6] = 0x99;
-	ret[5] = 0xaa;
-	ret[4] = 0xbb;
-	ret[3] = 0xcc;
-	ret[2] = 0xdd;
-	ret[1] = 0xee;
-	ret[0] = 0xff;
+	ret[0] = 0x00;
+	ret[1] = 0x11;
+	ret[2] = 0x22;
+	ret[3] = 0x33;
+	ret[4] = 0x44;
+	ret[5] = 0x55;
+	ret[6] = 0x66;
+	ret[7] = 0x77;
+	ret[8] = 0x88;
+	ret[9] = 0x99;
+	ret[10] = 0xaa;
+	ret[11] = 0xbb;
+	ret[12] = 0xcc;
+	ret[13] = 0xdd;
+	ret[14] = 0xee;
+	ret[15] = 0xff;
 	return (ret);
 }
 
@@ -89,7 +57,7 @@ int		main(void)
 	uint8_t	*key = set_key();
 	int		i;
 
-	printf("key:\n");
+	printf("ENCRYPTION:\nkey:\n");
 	for (i = 0; i < 16; ++i)
 		printf("%s%02X", i ? " | " : "", key[i]);
 	printf("\nBefore:\n");
@@ -99,10 +67,16 @@ int		main(void)
 	data = aes(data, key);
 	for (i = 0; i < 16; ++i)
 		printf("%s%02X", i ? " | " : "", data[i]);
-	/* printf("\nDECRYPTED:\n");*/
-	/* data = unaes(data, key);*/
-	/* for (i = 0; i < 16; ++i)*/
-	/*     printf("%s%02X", i ? " | " : "", data[i]);*/
+	printf("\n\nDECRYPTION:\nkey:\n");
+	for (i = 0; i < 16; ++i)
+		printf("%s%02X", i ? " | " : "", key[i]);
+	printf("\nBefore:\n");
+	for (i = 0; i < 16; ++i)
+		printf("%s%02X", i ? " | " : "", data[i]);
+	printf("\nAfter:\n");
+	data = unaes(data, key);
+	for (i = 0; i < 16; ++i)
+		printf("%s%02X", i ? " | " : "", data[i]);
 	printf("\n");
 	return (0);
 }
